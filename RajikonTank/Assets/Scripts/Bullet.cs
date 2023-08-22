3583b5b7b652b0a64cb4f57bdb5a276b390de830
sfslime <93108188+sfslime@users.noreply.github.com>
2023-08-22 14:34:51 +0900

    弾を発射処理

---
+++ RajikonTank/Assets/Scripts/Bullet.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] GameObject Tank;
+
+    [SerializeField] float BulletSpeed;               // 弾の移動スピード.
+    [SerializeField, Range(0, 2)] int MaxReflect;     // 最大反射回数.
+    [SerializeField] float CntReflect;                // 反射した回数.
+
+    private void OnDisable()
+    {
+        transform.position = Tank.transform.position;
+        CntReflect = 0;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        CntReflect = 0;
+        BulletActive(false);
+        BulletMove();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 弾のON・OFF.
+    /// </summary>
+    void BulletActive(bool isswitch)
+    {
+        if (isswitch)
+        {
+            gameObject.SetActive(true);
+        }
+        else if (!isswitch)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+    /// <summary>
+    /// 弾の移動関数.
+    /// </summary>
+    private void BulletMove()
+    {
+        rb.velocity = Tank.transform.forward * BulletSpeed * Time.deltaTime;
+    }
+}
