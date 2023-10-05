d1a59ccfe47c19b4a5911d861cd5e85283d8d957
HirotoNagatsuka <93108443+HirotoNagatsuka@users.noreply.github.com>
2023-10-05 15:43:53 +0900

    CPUを座標を引数にしての生成に変更

--- RajikonTank/Assets/Scripts/Nagatsuka/StageManager.cs
+++ RajikonTank/Assets/Scripts/Nagatsuka/StageManager.cs
@@ -13,7 +13,7 @@ public class StageManager : MonoBehaviour
     [SerializeField] GameObject SpawnPoints;//タンクを生成する座標を入れたリスト.
     [SerializeField] GameObject[] tanks;
 
-    [SerializeField] GameObject Enemys;//このオブジェクトの子供要素にCPUを生成する(旧Tanks).
+    [SerializeField] EnemyManager enemyManager;//このオブジェクトの子供要素にCPUを生成する(旧Tanks).
 
     [Header("確認用変数")]
     [SerializeField]int ChindCnt;
@@ -25,6 +25,7 @@ public class StageManager : MonoBehaviour
         for(int i = 0; i < ChindCnt; i++)           //子オブジェクトの数分ループしてタンクを生成する.
         {
             teamID = SpawnPoints.transform.GetChild(i).gameObject.GetComponent<SpawnPoint>().teamID;//ID取得.
+            Debug.Log(teamID);
             CreateTank(teamID, SpawnPoints.transform.GetChild(i).gameObject.transform.position);    //タンク生成関数.
         }
     }
@@ -38,16 +39,15 @@ public class StageManager : MonoBehaviour
         GameObject tank;
         switch (teamID) {
             case TeamID.player:
-                tank = Instantiate(tanks[PLAYER_NUM], position, Quaternion.identity);
-                //tank = TankGenerateClass.TankInstantiate(TankPrefabNames.TANK_NORMAL);//Playerをパス指定で生成.
+                tank = TankGenerateClass.TankInstantiate(TankPrefabNames.NONE);//Playerをパス指定で生成.
                 tank.transform.position = position;                                     //生成位置をセット.
-                GameManager.instance.PushTank(teamID, tank.GetComponent<Rajikon>());    //チームID送信
                 break;
             case TeamID.CPU:
-                tank = TankGenerateClass.TankInstantiate(TankPrefabNames.TANK_NORMAL);//CPUをパス指定で生成.
-                tank.transform.position = position;                                   //生成位置をセット.
-                tank.transform.parent = Enemys.transform;                             //CPUをEmemysの子供に.
-                GameManager.instance.PushTank(teamID, tank.GetComponent<Rajikon>());  //チームID送信
+                //enemyManager.SpawnNormalEnemy(position);
+                //tank = TankGenerateClass.TankInstantiate(TankPrefabNames.Enemy_Normal);//CPUをパス指定で生成.
+                //tank.transform.position = position;                                    //生成位置をセット.
+                //tank.transform.parent = enemyManager.transform;                              //CPUをEmemysの子供に.
+                //GameManager.instance.PushTank(teamID, tank.GetComponent<Rajikon>());   //チームID送信
                 break;
         }
        // GameManager.instance.testOBJ = GameManager.instance.teamInfo[GameManager.instance.player_IDnum].tankList[0].gameObject;
