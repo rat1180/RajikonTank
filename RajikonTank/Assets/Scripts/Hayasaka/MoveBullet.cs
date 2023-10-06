35348614ba811f670733b45e58ca60fc081ccc58
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2023-10-06 09:01:15 +0900

    ターゲット指定方法の変更

--- RajikonTank/Assets/Scripts/Hayasaka/MoveBullet.cs
+++ RajikonTank/Assets/Scripts/Hayasaka/MoveBullet.cs
@@ -15,6 +15,8 @@ public class MoveBullet : MonoBehaviour
     Vector3 Direction;
     Vector3 TestStartPos;
     Vector3 TestTarget;
+
+
     // Start is called before the first frame update
     void Start()
     {
