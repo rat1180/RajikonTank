14325f8ad0024584fb85412b56c4a16d5e90a91e
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2023-10-20 10:21:59 +0900

    フェードインアウトの設定

---
+++ RajikonTank/Assets/Scripts/Hida/ImageEffect.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageEffect : MonoBehaviour
+{
+    [SerializeField, Header("処理で使用する変数"), Tooltip("透明値")] private float ClearlanceNm;
+    [SerializeField, Tooltip("アニメーションさせるイメージコンポーネント")] private Image Image;
+    [SerializeField, Tooltip("フェードのスピード"), Range(0.001f, 0.01f)] public float FadeSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 設定したイメージにフェードインかアウトを行う
+    /// fadetimeに設定した時間に合うようにアウトインを行う
+    /// isfadeoutをtrueにすればフェードアウトになる
+    /// </summary>
+    /// <param name="fadetime"></param>
+    /// <param name="isfadeout"></param>
+    /// <returns></returns>
+    public IEnumerator ImageFadeInAndOut(float fadetime,bool isfadeout)
+    {
+        float time = 0;
+        int fadedirection = isfadeout ? -1 : 1;
+        float end = isfadeout ? 0 : 1;
+        ClearlanceNm = Image.color.a;
+
+        while (true)
+        {
+            time += Time.deltaTime;
+
+            ClearlanceNm += fadedirection * FadeSpeed;
+
+            Image.color = new Color(Image.color.r, Image.color.g, Image.color.b, ClearlanceNm);
+
+            if (isfadeout)
+            {
+                if (ClearlanceNm <= end) yield break;
+            }
+            else
+            {
+                if (ClearlanceNm >= end) yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+    public void DefaultFadeInAndOut(bool isfadeout)
+    {
+        StartCoroutine(ImageFadeInAndOut(0,isfadeout));
+    }
+}
