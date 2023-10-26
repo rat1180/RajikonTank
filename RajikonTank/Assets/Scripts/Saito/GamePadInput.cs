c1cde742b45321db8222242d48670d1ea17117f5
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2023-10-26 12:23:07 +0900

    デバッグ

--- RajikonTank/Assets/Scripts/Saito/GamePadInput.cs
+++ RajikonTank/Assets/Scripts/Saito/GamePadInput.cs
@@ -11,7 +11,7 @@ public class GamePadInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gamepad = Gamepad.current;
     }
 
     // Update is called once per frame
