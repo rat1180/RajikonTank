11025c05470e36712396c9125f357823cfab7ae0
HirotoNagatsuka <93108443+HirotoNagatsuka@users.noreply.github.com>
2023-11-10 09:14:49 +0900

    Stageのスクリプトの使用を変更

--- RajikonTank/Assets/Scripts/Nagatsuka/StageManager.cs
+++ RajikonTank/Assets/Scripts/Nagatsuka/StageManager.cs
@@ -12,17 +12,20 @@ public class StageManager : MonoBehaviour
     const int SPOWN_POINTS = 0;
     #endregion
 
-    [SerializeField] bool createFlg;//Playerを生成したかを判定する.
-
-    //public GameObject Stages;//Stagesをヒエラルキーから入れる.
-    //public List<GameObject> Stage;
-    public GameObject Stage;//private
-    [SerializeField] GameObject[] tanks;    //テスト用プレファブ.
-
-    [SerializeField] EnemyManager enemyManager;//このオブジェクトの子供要素にCPUを生成する.
-
+    #region private変数
     private int ChindCnt;//SpawnPointsの子供要素の数を数える用変数.
+    private bool createFlg;//Playerを生成したかを判定する.
+    private GameObject Stage;
+    #endregion
 
+    [SerializeField] GameObject playerTank; //Playerプレファブ.
+
+    [Tooltip("Enemysを入れる"), SerializeField]
+    EnemyManager enemyManager;//このオブジェクトの子供要素にCPUを生成する.
+
+    /// <summary>
+    /// SpawnPointsの子要素を取得し、IDに応じてタンクを生成する.
+    /// </summary>
     private void GetSpawnID(GameObject spawnPoints)
     {
         ChindCnt = spawnPoints.transform.childCount;//子供の数を取得する.
@@ -30,24 +33,41 @@ public class StageManager : MonoBehaviour
         EnemyName enemyName;                        //CPUの場合名前も取得.
         for (int i = 0; i < ChindCnt; i++)          //子オブジェクトの数分ループしてタンクを生成する.
         {
-            teamID = spawnPoints.transform.GetChild(i).gameObject.GetComponent<SpawnPoint>().teamID;//ID取得.
-            enemyName = spawnPoints.transform.GetChild(i).gameObject.GetComponent<SpawnPoint>().enemyName;//ID取得.
-            switch (teamID) {
+            teamID = spawnPoints.transform.GetChild(i).gameObject.GetComponent<SpawnPoint>().teamID;      //チームID取得.
+            enemyName = spawnPoints.transform.GetChild(i).gameObject.GetComponent<SpawnPoint>().enemyName;//名前取得.
+            switch (teamID)
+            {
                 case TeamID.player:
-                    CreateTank(spawnPoints.transform.GetChild(i).gameObject.transform.position);    //タンク生成関数.
+                    CreateTank(spawnPoints.transform.GetChild(i).gameObject.transform.position);            //タンク生成関数.
                     break;
                 case TeamID.CPU:
-                    CreateTank(enemyName, spawnPoints.transform.GetChild(i).gameObject.transform.position);    //タンク生成関数.
+                    CreateTank(enemyName, spawnPoints.transform.GetChild(i).gameObject.transform.position); //タンク生成関数.
                     break;
                 default:
                     Debug.LogError("生成に問題発生");
                     break;
-            }  
+            }
         }
-        spawnPoints.SetActive(false);
-        GameManager.instance.NowGameState = GAMESTATUS.READY;
+        spawnPoints.SetActive(false);                        //使い終わったら目印を消すためにSpawnPointsを非表示にする.
+        GameManager.instance.NowGameState = GAMESTATUS.READY;//全てのタンクの生成が終わったらReady状態にする.
     }
 
+    /// <summary>
+    /// GameManagerから呼び出す関数
+    /// 引数にアクティブにするステージナンバーを指定、スポーンポイントを取得しタンクを生成する.
+    /// </summary>
+    public void ActiveStage(int stage)
+    {
+        GameObject spawnPoints;
+        GameObject previousStage;//前回のステージ破棄用.
+        previousStage = Stage;
+        Destroy(previousStage);
+        Stage = Instantiate((ResorceManager.Instance.GetStageResorce((StageNames)stage)));//Stageを生成し、変数に代入する.
+        spawnPoints = Stage.transform.GetChild(SPOWN_POINTS).gameObject;
+        GetSpawnID(spawnPoints);
+    }
+
+    #region CreateTank(Player・CPU用)
     /// <summary>
     /// Playerタンクを生成する関数
     /// 引数にIDと生成する座標を指定.
@@ -55,23 +75,25 @@ public class StageManager : MonoBehaviour
     void CreateTank(Vector3 position)
     {
 
-        if (createFlg)
+        if (createFlg)//既に生成していたら位置を変えるだけ.
         {
-            GameManager.instance.teamInfo[GameManager.instance.player_IDnum].SetPosition(0,position);
-            //GameManager.instance.teamInfo[GameManager.instance.player_IDnum].SetRotation(0, child);
+            GameManager.instance.teamInfo[GameManager.instance.player_IDnum].SetPosition(0, position);
             GameManager.instance.teamInfo[GameManager.instance.player_IDnum].
                 tankList[GameManager.instance.OWN_playerID].SetPlayTrail(false);
         }
         else
         {
             GameObject tank;
-            tank = Instantiate(tanks[PLAYER_NUM], position,Quaternion.identity);
-            //tank.transform.LookAt(child.transform.position);
-
+            tank = Instantiate(playerTank, position, Quaternion.identity);
             tank.GetComponent<PlayerClass>().InitPlayer(PlayerClass.InitMode.NATURAL);
             createFlg = true;
         }
     }
+
+    /// <summary>
+    /// 敵生成用のCreateTank
+    /// 敵の名前と座標を引数に指定された敵を生成する
+    /// </summary>
     void CreateTank(EnemyName name, Vector3 position)
     {
         switch (name)
@@ -84,7 +106,7 @@ public class StageManager : MonoBehaviour
                 break;
             case EnemyName.MOVEMENT:
                 enemyManager.SpawnEnemy(position, TankPrefabNames.Enemy_Movement);//EnemyManagerの生成関数を呼び出す.
-                break;       
+                break;
             case EnemyName.FASTBULLET:
                 enemyManager.SpawnEnemy(position, TankPrefabNames.Enemy_FastBullet);//EnemyManagerの生成関数を呼び出す.
                 break;
@@ -93,33 +115,6 @@ public class StageManager : MonoBehaviour
                 break;
         }
     }
+    #endregion
 
-    /// <summary>
-    /// GameManagerから呼び出す関数
-    /// 引数にアクティブにするステージナンバーを指定、スポーンポイントを取得しタンクを生成する.
-    /// </summary>
-    /// <param name="stage"></param>
-    public void ActiveStage(int stage)
-    {
-        GameObject spawnPoints;
-        GameObject previousStage;//前回のステージ破棄用.
-        previousStage = Stage;
-        Destroy(previousStage);
-        Stage = Instantiate((ResorceManager.Instance.GetStageResorce((StageNames)stage)));
-        spawnPoints = Stage.transform.GetChild(SPOWN_POINTS).gameObject;
-        GetSpawnID(spawnPoints);
-        //for (int i = 0; i < Stage.Count; i++)
-        //{
-        //    if(i == stage)
-        //    {
-        //        Stage[i].SetActive(true);
-        //        spawnPoints = Stage[i].transform.GetChild(SPOWN_POINTS).gameObject;
-        //        GetSpawnID(spawnPoints);
-        //    }
-        //    else
-        //    {
-        //        Stage[i].SetActive(false);
-        //    }
-        //}
-    }
 }
